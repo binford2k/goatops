0f95bb5b3a5bb5aab187fceaa927e1e00dc06839
Ben Ford <ben.ford@puppetlabs.com>
2016-05-28 15:03:43 -0700

    Updated styles
    * list only 5 random reasons at a time for a less daunting list.
    * added "full list" link to see the whole thing
    * pinned credits to bottom of page

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 $( document ).ready(function() {
+
   // shuffle the list
   $('ul').each(function() {
     var list  = $(this);
     var items = list.children();
+    var count = items.length;
 
-    while (items.length) {
-        list.append(items.splice(Math.floor(Math.random() * items.length), 1)[0]);
-    }
+    // How many items we want in the final list
+    var size  = (window.location.search == '?full') ? count : 5;
+
+    list.empty();
+
+    for (i = 0; i < size; i++) {
+      list.append(items.splice(Math.floor(Math.random() * count), 1)[0]);
+    };
+
   });
 
 });
